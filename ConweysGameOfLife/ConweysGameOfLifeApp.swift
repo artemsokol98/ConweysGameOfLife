ea91bdd7c443080bf2138aa4b43917fa09993cdb
Artem Sokolovskiy <artemsokol98@gmail.com>
2023-01-09 20:31:39 +0300

    Initial Commit

---
+++ ConweysGameOfLife/ConweysGameOfLifeApp.swift
@@ -0,0 +1,20 @@
+//
+//  ConweysGameOfLifeApp.swift
+//  ConweysGameOfLife
+//
+//  Created by Артем Соколовский on 09.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ConweysGameOfLifeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
